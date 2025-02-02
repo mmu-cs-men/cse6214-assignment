3b605d845d1688f6a2514bc1b51433c1545d4394
Harris Majeed <30569190+XTapped@users.noreply.github.com>
2025-02-03 04:12:14 +0800

    add apps to settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "admin",
+    "buyer",
+    "seller",
+    "courier",
+    "core",
 ]
 
 MIDDLEWARE = [
