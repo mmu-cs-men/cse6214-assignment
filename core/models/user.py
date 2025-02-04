1bb0af231f8f6cc3c2c34ff60fbe77e7d12a6c8f
Laxman Pillai <laxman.pillai1104@gmail.com>
2025-02-05 01:23:01 +0800

    Lint Shit

--- core/models/user.py
+++ core/models/user.py
@@ -5,21 +5,21 @@ from core.constants import ROLE_CHOICES
 
 class User(models.Model):
     """
-       Represents a user in the system.
+    Represents a user in the system.
 
-       This model defines the attributes and roles of users within the platform. Each user
-       has a unique email, a name, and a specific role that determines their permissions
-       and actions.
+    This model defines the attributes and roles of users within the platform. Each user
+    has a unique email, a name, and a specific role that determines their permissions
+    and actions.
 
-       :ivar email: A unique email address used for authentication and identification.
-       :vartype email: str
+    :ivar email: A unique email address used for authentication and identification.
+    :vartype email: str
 
-       :ivar name: The full name of the user.
-       :vartype name: str
+    :ivar name: The full name of the user.
+    :vartype name: str
 
-       :ivar role: The role of the user within the system, chosen from predefined options
-           ('buyer', 'seller', 'admin', 'courier'). Defaults to 'buyer'.
-       :vartype role: str
+    :ivar role: The role of the user within the system, chosen from predefined options
+        ('buyer', 'seller', 'admin', 'courier'). Defaults to 'buyer'.
+    :vartype role: str
        """
 
     email = models.EmailField(unique=True, null=False, blank=False)
