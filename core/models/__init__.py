12074d24049434e140b992a46bd08bf34c911094
Laxman Pillai <laxman.pillai1104@gmail.com>
2025-02-04 17:08:47 +0800

    Create OrderItem model and add related tests

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,6 +2,7 @@ from .book_listing import BookListing
 from .cart import Cart
 from .cart_item import CartItem
 from .order import Order
+from .order_item import OrderItem
 from .review import Review
 from .shop import Shop
 from .upgrade_request import UpgradeRequest
