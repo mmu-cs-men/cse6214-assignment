bde73a8f4a20a2f0644b154fb7f93e4aca8ee94e
Abdullah Hawash <abdullahhawash93@gmail.com>
2025-02-08 07:03:37 +0800

    Add logout and courier URLs to urlpatterns

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("buyer/", include("buyer.urls")),
+    path("courier/", include("courier.urls")),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),  # logout URL
 ]
 
 if settings.DEBUG:
