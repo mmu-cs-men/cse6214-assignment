9489fb8ce1417d688447ca34d04cfaab68599438
Laxman Pillai <laxman.pillai1104@gmail.com>
2025-02-08 01:30:14 +0800

    Update import order for buyer view modules

--- buyer/views/__init__.py
+++ buyer/views/__init__.py
@@ -2,9 +2,10 @@
 Exposes buyer view functions.
 """
 
+from .book_details import book_details_page
 from .cart import cart_page
 from .checkout import checkout_page
 from .landing import landing_page
-from .orders import orders_page
 from .order_details import order_details_page
+from .orders import orders_page
 from .orders import orders_page
