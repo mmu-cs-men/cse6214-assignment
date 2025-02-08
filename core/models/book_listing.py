6cf70d372db4c9ee7f6f8c5470c33904fad74dd6
Laxman Pillai <laxman.pillai1104@gmail.com>
2025-02-09 01:40:40 +0800

    migration issues

--- core/models/book_listing.py
+++ core/models/book_listing.py
@@ -26,7 +26,7 @@ class BookListing(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="book_images/", null=True, blank=True)
     bought = models.BooleanField(default=False)
-    description = models.TextField(blank=True, null=True)
+    descriptions = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
