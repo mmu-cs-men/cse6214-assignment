75fbe84513d448c3bbb6cc76f187f1b4cacfb1ab
Abdullah Hawash <abdullahhawash93@gmail.com>
2025-02-08 21:28:27 +0800

    Update courier URL patterns to add accept order view

--- courier/urls.py
+++ courier/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 
-from .views import deliveries_page, update_delivery, report_issue
+from .views import deliveries_page, accept_order, update_assignment, report_issue
 
 urlpatterns = [
-    path("", deliveries_page, name="courier-index"),  # default landing page
-    path("deliveries/", deliveries_page, name="courier-deliveries"),
+    path("", deliveries_page, name="courier-deliveries"),
+    path("accept/<int:order_id>/", accept_order, name="courier-accept-order"),
     path(
-        "deliveries/update/<int:assignment_id>/",
-        update_delivery,
-        name="courier-update-delivery",
+        "update/<int:assignment_id>/",
+        update_assignment,
+        name="courier-update-assignment",
     ),
-    path(
-        "deliveries/report/<int:assignment_id>/",
-        report_issue,
-        name="courier-report-issue",
-    ),
+    path("report/<int:assignment_id>/", report_issue, name="courier-report-issue"),
 ]
