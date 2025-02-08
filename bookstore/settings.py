2d9909c7f984256f7ccf2c1fb7dcf0f247aaafdd
Harris Majeed <30569190+XTapped@users.noreply.github.com>
2025-02-08 18:49:22 +0800

    add default redirect & session invalidation

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -125,3 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/"
+SESSION_COOKIE_AGE = 300
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
