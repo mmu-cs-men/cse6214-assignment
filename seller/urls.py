10e8d5f7dec711beec33b63cfbc4522424dd4632
Abdullah Hawash <abdullahhawash93@gmail.com>
2025-02-09 23:52:24 +0800

    Fix URL name inconsistencies in seller

--- seller/urls.py
+++ seller/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from seller.views import *
 from .views.orders import orders_page, mark_order_ready
 
@@ -18,6 +19,6 @@ urlpatterns = [
     ),
     path("profile/", profile_page, name="seller-profile"),
     path("update-shop-name/", update_shop_name, name="update-shop-name"),
-    path("orders/", orders_page, name="seller_orders"),
-    path("orders/<int:order_id>/ready/", mark_order_ready, name="mark_order_ready"),
+    path("orders/", orders_page, name="seller-orders"),
+    path("orders/<int:order_id>/ready/", mark_order_ready, name="mark-order-ready"),
 ]
