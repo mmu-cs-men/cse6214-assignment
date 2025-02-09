ab26434ef50020172d6238ddfedaf4dea6674749
Harris Majeed <30569190+XTapped@users.noreply.github.com>
2025-02-10 03:03:28 +0800

    formatting

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,6 @@
 from core.models.user import User
 
+
 def user_data(request):
     """
     Context processor to add user data to all templates
@@ -7,7 +8,7 @@ def user_data(request):
     if request.user.is_authenticated:
         try:
             user = User.objects.get(email=request.user.email)
-            return {'custom_user': user}
+            return {"custom_user": user}
         except User.DoesNotExist:
             return {}
-    return {} 
+    return {}
