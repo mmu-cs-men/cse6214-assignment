72ad51d1f0b967f3d2535f6c9e64da59828ea52f
Harris Majeed <30569190+XTapped@users.noreply.github.com>
2025-02-11 05:24:55 +0800

    feat: add cdn (not used yet)

---
+++ core/utils/cdn.py
@@ -0,0 +1,8 @@
+from imagekitio import ImageKit
+
+
+imagekit = ImageKit(
+    private_key="private_+kRnJCZfoiTkm6WmQmwXlxrx4ew=",
+    public_key="public_k1uw6aQxX2FvkMWGqe/yFK4g1fU=",
+    url_endpoint="https://ik.imagekit.io/softwareengbookstore",
+)
