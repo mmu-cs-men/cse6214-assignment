3970a00e8d3240c8747f4ad6e3f7e02322daac9e
Harris Majeed <30569190+XTapped@users.noreply.github.com>
2025-02-11 07:04:44 +0800

    refactor: consolidate ImageKit configuration in imagekit_utils.py

--- core/utils/imagekit_utils.py
+++ core/utils/imagekit_utils.py
@@ -1,5 +1,11 @@
-from image_cdn import imagekit
 from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+from imagekitio import ImageKit
+
+imagekit = ImageKit(
+    private_key="private_+kRnJCZfoiTkm6WmQmwXlxrx4ew=",
+    public_key="public_k1uw6aQxX2FvkMWGqe/yFK4g1fU=",
+    url_endpoint="https://ik.imagekit.io/softwareengbookstore",
+)
 
 
 def upload_file_to_imagekit(file, filename):
